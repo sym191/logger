8bb1fc43dc600d841ea49e64bcc042d015bc04ad
sym191 <ymsun.swjtu@gmail.com>
2025-08-11 00:10:50 +0800

    移除多余提交

--- buffer.cpp
+++ buffer.cpp
@@ -1,21 +1,21 @@
-//
-// Created by anon on 25-8-9.
-//
+// //
+// // Created by anon on 25-8-9.
+// //
 
-#include "buffer.h"
+// #include "buffer.h"
 
-void buffer::push(const std::string& filename, const std::string& message)
-{
-    _buffer.emplace_back(message, filename);
-    if (message.size() > _data.capacity() - _cur_size)
-    {
-        _data.reserve(_cur_size + message.size());
-    }
-    memcpy(_data.data() + _cur_size, message.data(), message.size());
-    _cur_size += message.size();
-}
+// void buffer::push(const std::string& filename, const std::string& message)
+// {
+//     _buffer.emplace_back(message, filename);
+//     if (message.size() > _data.capacity() - _cur_size)
+//     {
+//         _data.reserve(_cur_size + message.size());
+//     }
+//     memcpy(_data.data() + _cur_size, message.data(), message.size());
+//     _cur_size += message.size();
+// }
 
-void buffer::push(buffer& buf)
-{
+// void buffer::push(buffer& buf)
+// {
 
-}
+// }
